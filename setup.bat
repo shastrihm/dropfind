061b6c927e8d427bb778a7da3f83d72e77f1721c
shastrihm <hrishees@gmail.com>
2021-11-04 16:31:58 -0700

    update setup

--- setup.bat
+++ setup.bat
@@ -4,4 +4,4 @@ pip3 install tensorflow==2.6.0
 pip3 install Cython==0.29.24
 pip3 install git+https://github.com/philferriere/cocoapi.git#subdirectory=PythonAPI
 python -m pip install ./research/object_detection/packages/tf2
-pip3 install watchdog==2.1.5
+pip3 install keras==2.6.*
